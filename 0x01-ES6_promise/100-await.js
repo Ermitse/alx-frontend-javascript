6fda1b1340e7a3706660d3e7eb11bd2c8692fd4b
root <Ermitse@github.com>
2023-06-30 21:37:00 +0000

    async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
-const asyncUploadUser = async () => {
+export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-
-    return { photo, user };
+    return {
+      photo,
+      user,
+    };
   } catch (error) {
-    return { photo: null, user: null };
+    return {
+      photo: null,
+      user: null,
+    };
   }
-};
+}
